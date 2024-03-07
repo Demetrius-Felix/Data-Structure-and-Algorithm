e9a1a640f62ca3082eee33fc00dbb14efffdcfe5
Demetrius-Felix <158720701+Demetrius-Felix@users.noreply.github.com>
2024-03-07 13:31:35 +0800

    Update stack.cpp

--- 栈/stack.cpp
+++ 栈/stack.cpp
@@ -1,1 +1,47 @@
+#include<iostream>
+#include<string.h>
+#define MAXSIZE 100
+#define OK 1
+#define ERROR 0
+using namespace std;
 
+typedef struct{
+    int *base;
+    int *top;
+    int stacksize;
+}stack;
+
+void Initstack(stack &s){
+    s.top=s.base=new int[MAXSIZE];
+    s.stacksize=MAXSIZE;
+}
+
+int push(stack &s,int e){
+    if(s.top-s.base>=s.stacksize)return ERROR;
+    *s.top++=e;
+    return OK;
+}
+
+int pop(stack &s){
+    if(s.top==s.base)return ERROR;
+    cout<<*--s.top<<" ";
+    return OK;
+}
+
+int main(){
+    int m,n;
+    stack s;
+    Initstack(s);
+    cin>>m;
+    for(int i=0;i<m;i++){
+        int e;
+        cin>>e;
+        push(s,e);
+    }
+    cin>>n;
+    for(int i=0;i<n;i++){
+        pop(s);
+    }
+    cout<<*(s.top-1)<<endl;
+    return 0;
+}
